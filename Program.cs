d5781d6d61fd82965fc3a8d17200e381c673b8e3
Rodrigo Moyano <rodrigo.moyano@outlook.es>
2023-10-09 23:33:13 -0300

    Practica

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string modelo, marca, NumeroTelefonico, estado;
+            int CodigoOperador;
+
+            Telefono t1 = new Telefono("14", "Iphone");
+            Telefono t2 = new Telefono("Nuevo");
+
+            Console.WriteLine(t1.Llamar());
+            Console.WriteLine(t1.Llamar("Fulanito"));
+            Console.WriteLine("Elija la opcion 1, 2 o 3 caso contrario ingresar cero");
+            t1.CodigoOperador = int.Parse(Console.ReadLine());
+            Console.WriteLine("Opcion elegida " +  t1.CodigoOperador);
+            Console.WriteLine("El telefono es un " + t1.getMarca + " Modelo " + t1.getModelo); 
+            Console.WriteLine(t2.Estado);
+
+            
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Telefono.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Desafio1
+{
+    internal class Telefono
+    {
+        public Telefono(string modelo, string marca)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+        public Telefono(string estado) 
+        {
+            this.estado = estado;
+        }
+
+        private string modelo, marca, numerotelefonico, estado;
+        private int codigooperador;
+
+        public string getModelo
+        {
+            get { return modelo; }
+        }
+        public string getMarca
+        {
+            get { return marca; }
+        }
+        public string NumeroTelefonico
+        {
+            get { return numerotelefonico; }
+            set { numerotelefonico = value; }
+        }
+        public int CodigoOperador
+        {
+            get { return codigooperador;}
+            set { if (value == 1)
+                    codigooperador = 1;
+                else if (value == 2)
+                    codigooperador = 2;
+                else if (value == 3)
+                    codigooperador = 3;
+                else codigooperador = 0;
+                    codigooperador = value; }
+        }
+         public string Estado
+        {
+            get { return "El estado del telefono es: " + estado; }
+            set { estado = value; }
+        }
+        public string Llamar()
+        {
+            return "Realizando llamada...";
+        }
+        public string Llamar(string contacto)
+        {
+           return "LLamando a " + contacto;
+        }
+
+
+    }
+}
